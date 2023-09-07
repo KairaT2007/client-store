0a3fd30a56e68d70df5ac263b70a0727e0be8c88
KairaT2007 <kairat.keihatsu@gmail.com>
2023-09-07 14:30:38 +0600

    fin

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -13,7 +13,7 @@ const HomePage = async () => {
     <Container>
       <div className="space-y-10 pb-10">
         <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
-          <div style={{ backgroundImage: `url(https://play-images-prod-ctf.tech.tvnz.co.nz/api/v1/web/image/3CLEzO37tocxCoSmzzcxwx/2733ae58689b3e59e477a86d2da48a36/friends-showtile.png.2733ae58689b3e59e477a86d2da48a36.jpg?width=1200&height=630)` }} className="rounded-xl aspect-square md:aspect-[2.4/1] overflow-hidden bg-auto bg-center bg-no-repeat">
+          <div style={{ backgroundImage: `url(https://logodownload.org/wp-content/uploads/2019/08/funko-logo-4.png)` }} className="rounded-xl aspect-square md:aspect-[2.4/1] overflow-hidden bg-auto bg-center bg-no-repeat">
           </div>
         </div>
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = async () => {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">STORE</p>
+            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Funko.svg/1200px-Funko.svg.png" className="w-[79px]" alt="" />
           </Link>
           <MainNav data={categories} />
           <NavbarActions />
